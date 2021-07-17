49cdc6c140aeb722b11d8def45882eb2cb750102
Anton Romankov <doit.rant@gmail.com>
2021-07-17 13:53:55 +0300

    ignore targets

--- volume/target/rls/debug/deps/volume-21b87ab1e568bb37.d
+++
@@ -1,5 +0,0 @@
-/Users/tonyR/proj/rust/rust_course_homework/volume/target/rls/debug/deps/volume-21b87ab1e568bb37.rmeta: volume/src/lib.rs
-
-/Users/tonyR/proj/rust/rust_course_homework/volume/target/rls/debug/deps/volume-21b87ab1e568bb37.d: volume/src/lib.rs
-
-volume/src/lib.rs:
